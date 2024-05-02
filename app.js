12bffb449ef9c544bfe181c0654a3350cd45ecc2
Bobogeng <suirito@gmail.com>
2024-05-02 21:07:17 +0700

    Add body parser

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import express from "express";
 import routes from "./routes/index.js";
@@ -8,6 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
 app.use(routes);
